530405f93ab5d28dc18d53aea758a21857fd9016
Armageddoner <141986516+Armageddoner@users.noreply.github.com>
2025-11-11 20:18:26 -0500

    Enhance Touch class with persistent hitboxes
    Updated the Touch class to support persistent hitboxes and improved hit detection.
    
    Additionally, fixed checking if callback exists at line #76 inside the loop. This can be an expensive calculation, so I now have it check once and then iterate.

--- TouchClass.lua
+++ TouchClass.lua
@@ -1,11 +1,10 @@
---!strict
 local RunService = game:GetService("RunService")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local Touch = {}
 Touch.__index = Touch
 
-local Cleanup = require(ReplicatedStorage.SharedModules.Trove)
+local Cleanup = require("./Trove") -- change to whereever you placed the cleanup
 
 local RATE = 0.1 -- how often hitboxes should be parsed
 
@@ -15,7 +14,7 @@ export type TouchInfo = {
 	CFrame: CFrame,
 	Method: TouchMethod,
 	Radius: number?
-} | Part
+} | BasePart
 
 --[[
 	An abstraction of <code>workspace:GetPartsInRadius()</code>
@@ -25,19 +24,26 @@ export type TouchInfo = {
 ]]
 function Touch.new(Info: TouchInfo, Parameters: OverlapParams, Callback: (PartThatTouched: BasePart) -> ()?)
 	if not Info then error("No TouchInfo provided!") end
-	local self = {}
-	if typeof(Info) == "Instance" and Info:IsA("Part") then
+	local self = setmetatable({}, Touch)
+	self.Cleaner = Cleanup.new()
+	self.Callback = Callback
+	
+	local Persistent = false
+	
+	if typeof(Info) == "Instance" and Info:IsA("BasePart") then
 		self.Size = Info.Size
 		self.CFrame = Info.CFrame
 		self.Method = "Box"
+		self.Cleaner:Connect(Info.Destroying, function()
+			self:Destroy()
+		end)
+		Persistent = true
 	else
 		self.Size = Info.Size
 		self.CFrame = Info.CFrame
 		self.Method = Info.Method
 		self.Radius = Info.Radius
 	end
-	self.Callback = Callback
-	self.Cleaner = Cleanup.new()
 	local STEP = 0
 
 	self.Cleaner:Connect(RunService.Heartbeat, function(DeltaTime)
@@ -50,7 +56,13 @@ function Touch.new(Info: TouchInfo, Parameters: OverlapParams, Callback: (PartTh
 		
 		STEP = 0
 		
-		local BoundParts = nil 
+		if Persistent and Info then
+			self.Size = Info.Size * 1.05 -- 5% larger to detect things at directly at an edge of the part.
+			self.CFrame = Info.CFrame
+		end
+		
+		local BoundParts: {BasePart} = nil 
+		
 		if self.Method == "Box" then
 			BoundParts = workspace:GetPartBoundsInBox(
 				self.CFrame,
@@ -60,18 +72,26 @@ function Touch.new(Info: TouchInfo, Parameters: OverlapParams, Callback: (PartTh
 		elseif self.Method == "Radius" then
 			BoundParts = workspace:GetPartBoundsInRadius(self.CFrame.Position, self.Radius, Parameters)
 		end
-		for _, Part: BasePart in BoundParts do
-			if self.Callback and typeof(self.Callback) == "function" then
+		
+		if self.Callback and typeof(self.Callback) == "function" then
+			for _, Part: BasePart in BoundParts do
 				self.Callback(Part)
 			end
 		end
+		
 	end)
-	return setmetatable(self, Touch)
+	return self
 end
 
 -- Destroys the <code>Touch</code>.
 function Touch:Destroy()
 	self.Cleaner:Clean()
+	self.Radius = nil
+	self.CFrame = nil
+	self.Method = nil
+	self.Size = nil
+	self.Callback = nil
+	self.Cleaner = nil
 end
 
 return Touch
